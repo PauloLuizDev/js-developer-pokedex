fa6832afc17b93a2bf7426934911176f3d61026d
Paulo Luiz dos Santos Filho <pauloluiz@oulook.com>
2022-12-25 22:35:11 -0300

    Implementação de maodais para exibição de detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 const pokemonList = document.getElementById('pokemonList')
+const modal = document.getElementById('modalDetalhes')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
@@ -8,29 +9,64 @@ let offset = 0;
 function convertPokemonToLi(pokemon) {
     return `
     <div class="pokemon">
-    <div class="card m-2 ">
-      <div class="card-content ${pokemon.type} p-2">
+    <div class="card m-2">
+      <button class="card-content ${pokemon.type} p-2  border-0"  href="#" class="d-inline" data-bs-toggle="modal" data-bs-target="#${pokemon.name}">
         <div class="card-body">
           <div class="media d-flex">
             <div class="align-self-center">
-              <span class=" float-left"><img src="${pokemon.photo}" alt="${pokemon.name}" class="card-img-top pse-2 mt-4"></span>
+              <span class=" float-left"><img src="${pokemon.photo}" alt="${pokemon.name}" class="card-img-top"></span>
             </div>
             <div class="media-body text-right text-white ps-2">
-              <h1 class="text-capitalize h6">${pokemon.name}</h1>
+              <h2 class="text-capitalize h6 ">${pokemon.name} </h2>
               <span >#${pokemon.number}</span>
-              <div class="types list-group list-group-horizontal">
+              <div class="">
                 ${pokemon.types.map((type) => `<img src="./assets/icon/${type}.svg"
                 alt="${type}" class="icone p-1 m-0">`).join('')}
-            </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      </button>
     </div>
   </div>
 
 
     `
+}
+
+function  convertPokemonToDetails(pokemon){
+  return `
+  <div class="modal fade" id="${pokemon.name}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="card-content ${pokemon.type} p-2">
+      <div class="modal-content">
+        <div class="modal-header">
+          <div class="">
+          <h2 class="text-capitalize h6 pt-5">${pokemon.name}</h2>
+          ${pokemon.types.map((type) => `<img src="./assets/icon/${type}.svg"
+          alt="${type}" class="icone p-1 m-0 rounded-circle ${pokemon.type}">`).join('')}
+        </div>
+        
+        </div>
+        <div class="modal-body">
+          <div class="align-self-center">
+            <span class=" float-left"><img src="${pokemon.photo}" alt="${pokemon.name}" class="card-img-top pse-2 mt-4"></span>
+          </div>
+          <span >#${pokemon.number}</span>
+
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  `
 }
 /*<div class="card pokemon p-0" >
 <img src="${pokemon.photo}"
@@ -51,14 +87,21 @@ function convertPokemonToLi(pokemon) {
     </div>
 </div>*/
 
-function loadPokemonItens(offset, limit) {
+function loadPokemon(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
     })
 }
+function loadPokemonDetails(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+      const newHtml = pokemons.map(convertPokemonToDetails).join('')
+      pokemonList.innerHTML += newHtml
+  })
+}
 
-loadPokemonItens(offset, limit)
+loadPokemon(offset, limit)
+loadPokemonDetails(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
@@ -66,10 +109,15 @@ loadMoreButton.addEventListener('click', () => {
 
     if (qtdRecordsWithNexPage >= maxRecords) {
         const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+        loadPokemon(offset, newLimit)
+        loadPokemonDetails(offset, limit)
 
         loadMoreButton.parentElement.removeChild(loadMoreButton)
     } else {
-        loadPokemonItens(offset, limit)
+        loadPokemon(offset, limit)
+        loadPokemonDetails(offset, limit)
     }
 })
+
+/*  Em LoadpokemonItens
+    realizar um segundo getPokemons para acrescentar os detalhes do pokemon*/
